88abbec9589c4e280158b9605c8d29b4ded305db
kayu55 <iwanles109@gmail.com>
2025-08-27 12:18:48 +0700

    Add files via upload

--- ssh/sshac.sh
+++ ssh/sshac.sh
@@ -3,25 +3,11 @@
 clear
 
 # Baca nama pengguna dari config.json, pisahkan username dan tanggal expired, lalu hilangkan duplikat
-while read expired
-do
-AKUN="$(echo $expired | cut -d: -f1)"
-ID="$(echo $expired | grep -v nobody | cut -d: -f3)"
-exp="$(chage -l $AKUN | grep "Account expires" | awk -F": " '{print $2}')"
-status="$(passwd -S $AKUN | awk '{print $2}' )"
-if [[ $ID -ge 1000 ]]; then
-if [[ "$status" = "L" ]]; then
-printf "%-17s %2s %-17s %2s \n" "$AKUN" "$exp     " "LOCKED${NORMAL}"
-else
-printf "%-17s %2s %-17s %2s \n" "$AKUN" "$exp     " "UNLOCKED${NORMAL}"
-fi
-fi
-done < /etc/passwd
-JUMLAH="$(awk -F: '$3 >= 1000 && $1 != "nobody" {print $1}' /etc/passwd | wc -l)"
-
-echo -e "\e[97;1m ==================================== \e[0m"
-echo -e "\e[97;1m       SHOW SSH USER ACCOUNT          \e[0m"
-echo -e "\e[97;1m ==================================== \e[0m"
+useradd -e `date -d "$masaaktif days" +"%Y-%m-%d"` -s /bin/false -M $Login
+hariini=`date -d "0 days" +"%Y-%m-%d"`
+exp="$(chage -l $Login | grep "Account expires" | awk -F": " '{print $2}')"
+echo -e "$Pass\n$Pass\n"|passwd $Login &> /dev/null
+PID=`ps -ef |grep -v grep | grep sshws |awk '{print $2}'`
 i=1
 for user in $users; do
     echo "$i) $user"
@@ -31,10 +17,10 @@ done
 echo -e "\e[97;1m ==================================== \e[0m"
 echo ""
 # Minta pengguna memilih nomor
-read -p " Just input Number: " AKUN
+read -p " Just input Number: " number
 
 # Dapatkan username berdasarkan nomor yang dipilih
-selected_user=$(echo "$users" | sed -n "${AKUN}p")
+selected_user=$(echo "$users" | sed -n "${number}p")
 
 if [ -z "$selected_user" ]; then
     echo -e "\e[31;1m number is missing or incorrect\e[0m"
