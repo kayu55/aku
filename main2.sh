1080fd37def0119da9f45ecea89c49cb2e9431d9
kayu55 <iwanles109@gmail.com>
2025-09-03 01:54:35 +0700

    Add files via upload

--- main2.sh
+++ main2.sh
@@ -340,6 +340,33 @@ chmod 0600 /swapfile >/dev/null 2>&1
 swapon /swapfile >/dev/null 2>&1
 sed -i '$ i\/swapfile      swap swap   defaults    0 0' /etc/fstab
 
+echo -e " Memasang Swap 2 GB"
+
+    # Mengambil versi terbaru gotop
+gotop_latest="$(curl -s https://api.github.com/repos/xxxserxxx/gotop/releases | grep tag_name | sed -E 's/.*"v(.*)".*/\1/' | head -n 1)"
+gotop_link="https://github.com/xxxserxxx/gotop/releases/download/v$gotop_latest/gotop_v${gotop_latest}_linux_amd64.deb"
+
+    # Download & install gotop
+curl -sL "$gotop_link" -o /tmp/gotop.deb
+dpkg -i /tmp/gotop.deb >/dev/null 2>&1
+
+    # Membuat swap file 2GB
+dd if=/dev/zero of=/swapfile bs=1M count=2048
+mkswap /swapfile
+chown root:root /swapfile
+chmod 0600 /swapfile
+swapon /swapfile >/dev/null 2>&1
+
+# Tambahkan swap ke fstab agar aktif saat boot
+sed -i '$ i\/swapfile swap swap defaults 0 0' /etc/fstab
+
+# Sinkronisasi waktu dengan server Indonesia
+chronyd -q 'server 0.id.pool.ntp.org iburst'
+chronyc sourcestats -v
+chronyc tracking -v
+
+echo -e "Swap 2 GB berhasil dipasang"
+
 clear
 echo  ""
 echo  "Sukses Sayank..!!"
