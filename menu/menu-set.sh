127d73530e6d8eb040474d91aac02d362c3f8100
kayu55 <iwanles109@gmail.com>
2025-09-21 15:17:31 +0700

    Add files via upload

--- menu/menu-set.sh
+++ menu/menu-set.sh
@@ -12,14 +12,14 @@ echo -e " $BICyan┌────────────────────
 echo -e " $BICyan│${BIGreen}[${BIWhite}1${BIGreen}]${NC}  • Renew Cert"
 echo -e " $BICyan│${BIGreen}[${BIWhite}2${BIGreen}]${NC}  • Change Banner SSH "
 echo -e " $BICyan│${BIGreen}[${BIWhite}3${BIGreen}]${NC}  • Restart Banner SSH "
-echo -e " $BICyan│${BIGreen}[${BIWhite}4${BIGreen}]${NC}  • Info Bandwidth"
+#echo -e " $BICyan│${BIGreen}[${BIWhite}4${BIGreen}]${NC}  • Info Bandwidth"
 echo -e " $BICyan│${BIGreen}[${BIWhite}5${BIGreen}]${NC}  • Restart All Service "
-echo -e " $BICyan│${BIGreen}[${BIWhite}6${BIGreen}]${NC}  • Set Auto Reboot"
-echo -e " $BICyan│${BIGreen}[${BIWhite}7${BIGreen}]${NC}  • Set Jam Auto Reboot"
-echo -e " $BICyan│${BIGreen}[${BIWhite}8${BIGreen}]${NC}  • Reboot Vps"
-echo -e " $BICyan│${BIGreen}[${BIWhite}9${BIGreen}]${NC}  • Clear Log"
-echo -e " $BICyan│${BIGreen}[${BIWhite}10${BIGreen}]${NC} • Upgrade Versi New Xray"
-echo -e " $BICyan│${BIGreen}[${BIWhite}11${BIGreen}]${NC} • Batasi Limit Banwidth Di Vps"
+#echo -e " $BICyan│${BIGreen}[${BIWhite}6${BIGreen}]${NC}  • Set Auto Reboot"
+#echo -e " $BICyan│${BIGreen}[${BIWhite}6${BIGreen}]${NC}  • Set Jam Auto Reboot"
+echo -e " $BICyan│${BIGreen}[${BIWhite}6${BIGreen}]${NC}  • Reboot Vps"
+echo -e " $BICyan│${BIGreen}[${BIWhite}7${BIGreen}]${NC}  • Clear Log"
+#echo -e " $BICyan│${BIGreen}[${BIWhite}10${BIGreen}]${NC} • Upgrade Versi New Xray"
+#echo -e " $BICyan│${BIGreen}[${BIWhite}11${BIGreen}]${NC} • Batasi Limit Banwidth Di Vps"
 echo -e " $BICyan│${BIGreen}[${BIWhite}0${BIGreen}]${NC}  • Balik Ke Menu"
 echo -e " $BICyan└───────────────────────────────────────────────┘${NC}"
 echo -e ""
@@ -31,12 +31,12 @@ case $opt in
 3) clear ; /etc/init.d/dropbear restart ;;
 4) clear ; cek-bandwidth ;;
 5) clear ; restart ;;
-6) clear ; autoreboot ;;
-7)clear ; jam ;;
-8) clear ; reboot ;;
-9) clear ; clearlog ;;
-10) clear ; update-xray ;;
-11) clear ; set-bw ;;
+006) clear ; autoreboot ;;
+8887)clear ; jam ;;
+6) clear ; reboot ;;
+7) clear ; clearlog ;;
+100000) clear ; update-xray ;;
+10001) clear ; set-bw ;;
 00 | 0) clear ; menu ;;
 *) clear ; menu-set ;;
 esac
