11efe2a399c2f603d156a7e9b93640d495b59e24
kayu55 <iwanles109@gmail.com>
2025-09-21 16:07:56 +0700

    Add files via upload

--- menu/menu-set.sh
+++ menu/menu-set.sh
@@ -13,11 +13,11 @@ echo -e " $BICyan│${BIGreen}[${BIWhite}1${BIGreen}]${NC}  • Renew Cert"
 echo -e " $BICyan│${BIGreen}[${BIWhite}2${BIGreen}]${NC}  • Change Banner SSH "
 echo -e " $BICyan│${BIGreen}[${BIWhite}3${BIGreen}]${NC}  • Restart Banner SSH "
 #echo -e " $BICyan│${BIGreen}[${BIWhite}4${BIGreen}]${NC}  • Info Bandwidth"
-echo -e " $BICyan│${BIGreen}[${BIWhite}5${BIGreen}]${NC}  • Restart All Service "
+echo -e " $BICyan│${BIGreen}[${BIWhite}4${BIGreen}]${NC}  • Restart All Service "
 #echo -e " $BICyan│${BIGreen}[${BIWhite}6${BIGreen}]${NC}  • Set Auto Reboot"
 #echo -e " $BICyan│${BIGreen}[${BIWhite}6${BIGreen}]${NC}  • Set Jam Auto Reboot"
-echo -e " $BICyan│${BIGreen}[${BIWhite}6${BIGreen}]${NC}  • Reboot Vps"
-echo -e " $BICyan│${BIGreen}[${BIWhite}7${BIGreen}]${NC}  • Clear Log"
+echo -e " $BICyan│${BIGreen}[${BIWhite}5${BIGreen}]${NC}  • Reboot Vps"
+echo -e " $BICyan│${BIGreen}[${BIWhite}6${BIGreen}]${NC}  • Clear Log"
 #echo -e " $BICyan│${BIGreen}[${BIWhite}10${BIGreen}]${NC} • Upgrade Versi New Xray"
 #echo -e " $BICyan│${BIGreen}[${BIWhite}11${BIGreen}]${NC} • Batasi Limit Banwidth Di Vps"
 echo -e " $BICyan│${BIGreen}[${BIWhite}0${BIGreen}]${NC}  • Balik Ke Menu"
@@ -29,12 +29,12 @@ case $opt in
 1) clear ; certxray ;;
 2) clear ; nano /etc/issue.net ;;
 3) clear ; /etc/init.d/dropbear restart ;;
-4) clear ; cek-bandwidth ;;
-5) clear ; restart ;;
+99994) clear ; cek-bandwidth ;;
+4) clear ; restart ;;
 006) clear ; autoreboot ;;
 8887)clear ; jam ;;
-6) clear ; reboot ;;
-7) clear ; clearlog ;;
+5) clear ; reboot ;;
+6) clear ; clearlog ;;
 100000) clear ; update-xray ;;
 10001) clear ; set-bw ;;
 00 | 0) clear ; menu ;;
