4e8b0b49ed3cf07e8f75bff8110b0bb7cf796d68
kayu55 <iwanles109@gmail.com>
2026-01-19 14:40:42 +0700

    Add files via upload

--- upray.sh
+++ upray.sh
@@ -329,25 +329,21 @@ cat <<EOF> /etc/xray/config.json
                  "id": "${uuid}",
                  "alterId": 0
 #vmess
-### bis 2026-01-31
-},{"id": "6fa273a2-41bc-440d-ade1-cddea4d4306a","alterId": 0,"email": "bis"
-### ind4 2026-02-10
-},{"id": "0da2ffa8-d07b-40ca-9333-488d409bb74a","alterId": 0,"email": "ind4"
-### edy 2026-02-09
-},{"id": "aa6ebbcb-e491-412b-a324-1b82cc5ced4d","alterId": 0,"email": "edy"
-### bz 2026-02-04
-},{"id": "c1e99d0e-4a18-46fa-8cbb-6093e9a81731","alterId": 0,"email": "bz"
-### fi26 2026-02-01
-},{"id": "61db1844-da54-422f-9187-7faff4c6140f","alterId": 0,"email": "fi26"
-### im 2026-01-26
-},{"id": "8b05c488-1293-449b-9e56-e4ca69442bdc","alterId": 0,"email": "im"
-### am4d 2026-01-21
-},{"id": "ecdcb6ab-63df-42b3-9b0b-662e803f2d18","alterId": 0,"email": "am4d"
-### ituja 2026-01-19
-},{"id": "d63b895b-57f7-48e4-8c05-c567cf9036ab","alterId": 0,"email": "ituja"
-          }
-        ]
-      },
+### rez 2026-01-27
+},{"id": "5c196566-7263-489f-aa6b-2fca3f474d2e","alterId": 0,"email": "rez"
+### gasenusa 2026-01-25
+},{"id": "7c52fbc2-0ddd-4237-b598-3bcc82d354cb","alterId": 0,"email": "gasenusa"
+### okee 2026-02-10
+},{"id": "d6aa3c99-9f49-4de3-b4cc-3f5dd6c9d98e","alterId": 0,"email": "okee"
+### kaf 2026-02-06
+},{"id": "63be8897-abda-4e90-8168-b53c5cc07e1e","alterId": 0,"email": "kaf"
+### roni7 2026-01-24
+},{"id": "ce4944e6-99f0-40c3-ad5b-6db670f5b1c1","alterId": 0,"email": "roni7"
+### bangray1999 2026-02-20
+},{"id": "6ea8545a-9a5d-409e-b227-5756b1e484c8","alterId": 0,"email": "bangray1999"
+             }
+          ]
+       },
        "streamSettings":{
          "network": "ws",
             "wsSettings": {
@@ -406,22 +402,18 @@ cat <<EOF> /etc/xray/config.json
                  "id": "${uuid}",
                  "alterId": 0
 #vmessgrpc
-### bis 2026-01-31
-},{"id": "6fa273a2-41bc-440d-ade1-cddea4d4306a","alterId": 0,"email": "bis"
-### ind4 2026-02-10
-},{"id": "0da2ffa8-d07b-40ca-9333-488d409bb74a","alterId": 0,"email": "ind4"
-### edy 2026-02-09
-},{"id": "aa6ebbcb-e491-412b-a324-1b82cc5ced4d","alterId": 0,"email": "edy"
-### bz 2026-02-04
-},{"id": "c1e99d0e-4a18-46fa-8cbb-6093e9a81731","alterId": 0,"email": "bz"
-### fi26 2026-02-01
-},{"id": "61db1844-da54-422f-9187-7faff4c6140f","alterId": 0,"email": "fi26"
-### im 2026-01-26
-},{"id": "8b05c488-1293-449b-9e56-e4ca69442bdc","alterId": 0,"email": "im"
-### am4d 2026-01-21
-},{"id": "ecdcb6ab-63df-42b3-9b0b-662e803f2d18","alterId": 0,"email": "am4d"
-### ituja 2026-01-19
-},{"id": "d63b895b-57f7-48e4-8c05-c567cf9036ab","alterId": 0,"email": "ituja"
+### rez 2026-01-27
+},{"id": "5c196566-7263-489f-aa6b-2fca3f474d2e","alterId": 0,"email": "rez"
+### gasenusa 2026-01-25
+},{"id": "7c52fbc2-0ddd-4237-b598-3bcc82d354cb","alterId": 0,"email": "gasenusa"
+### okee 2026-02-10
+},{"id": "d6aa3c99-9f49-4de3-b4cc-3f5dd6c9d98e","alterId": 0,"email": "okee"
+### kaf 2026-02-06
+},{"id": "63be8897-abda-4e90-8168-b53c5cc07e1e","alterId": 0,"email": "kaf"
+### roni7 2026-01-24
+},{"id": "ce4944e6-99f0-40c3-ad5b-6db670f5b1c1","alterId": 0,"email": "roni7"
+### bangray1999 2026-02-20
+},{"id": "6ea8545a-9a5d-409e-b227-5756b1e484c8","alterId": 0,"email": "bangray1999"
              }
           ]
        },
