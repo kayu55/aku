9de122c3731332092dfb85d2fca4e528a66d3ca5
kayu55 <iwanles109@gmail.com>
2026-06-01 17:54:27 +0700

    Add files via upload

--- setup.sh
+++ setup.sh
@@ -263,7 +263,7 @@ sleep 1
 wget -q https://raw.githubusercontent.com/kayu55/aku/main/tools/ins-xray.sh && chmod +x ins-xray.sh && ./ins-xray.sh
 sleep 1
 echo -e "┌─────────────────────────────────────────┐"
-echo -e " \E[42;1;37m           >>> Install Backup <<<           \E[0m$NC"
+echo -e " \E[42;1;37m           >>> Install Backup <<<        \E[0m$NC"
 echo -e "└─────────────────────────────────────────┘"
 sleep 1
 wget -q https://raw.githubusercontent.com/kayu55/aku/main/set-br2.sh && chmod +x set-br2.sh && ./set-br2.sh
