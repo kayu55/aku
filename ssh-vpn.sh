fb9e5ac611b6a972b8a9f86a00bf40957e6ad5f1
kayu55 <iwanles109@gmail.com>
2026-09-07 10:00:00 +0700

    Add files via upload

--- ssh-vpn.sh
+++ ssh-vpn.sh
@@ -451,8 +451,8 @@ sleep 1
 echo -e "[ ${green}ok${NC} ] Restarting Fail2ban"
 /etc/init.d/fail2ban restart >/dev/null 2>&1
 sleep 1
-echo -e "[ ${green}ok${NC} ] Restarting Stunnel5"
-/etc/init.d/stunnel5 restart >/dev/null 2>&1
+echo -e "[ ${green}ok${NC} ] Restarting Stunnel4"
+/etc/init.d/stunnel4 restart >/dev/null 2>&1
 sleep 1
 echo -e "[ ${green}ok${NC} ] Restarting Vnstat"
 /etc/init.d/vnstat restart >/dev/null 2>&1
