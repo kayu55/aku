25f54272455c4b741589876459ec98d65ba8ea84
kayu55 <iwanles109@gmail.com>
2026-09-21 15:42:01 +0700

    Add files via upload

--- trialtrojan.sh
+++ trialtrojan.sh
@@ -6,9 +6,22 @@ MYIP=$(curl -sS ipv4.icanhazip.com)
 clear
 domain=$(cat /etc/xray/domain)
 #tr="$(cat ~/log-install.txt | grep -w "Trojan WS" | cut -d: -f2|sed 's/ //g')"
-user=trialtro`</dev/urandom tr -dc X-Z0-9 | head -c4`
+if ! command -v at &> /dev/null; then
+    apt update && sudo apt install -y at
+	systemctl enable --now atd
+fi
+Login="${1:-Trial}"        
+masaaktif="${2:-1}"        
+iplimit="${3:-1}"
+pup=30
+user="${Login}tr$(tr -dc 0-9 </dev/urandom | head -c3)"
+IP=$(curl -sS ipv4.icanhazip.com)
+ISP=$(cat /etc/xray/isp)
+CITY=$(cat /etc/xray/city)
+domain=$(cat /etc/xray/domain)
+nama=$(cat /etc/xray/username)
 uuid=$(cat /proc/sys/kernel/random/uuid)
-masaaktif=1
+
 exp=`date -d "$masaaktif days" +"%Y-%m-%d"`
 sed -i '/#trojanws$/a\### '"$user $exp"'\
 },{"password": "'""$uuid""'","email": "'""$user""'"' /etc/xray/config.json
@@ -18,6 +31,9 @@ sed -i '/#trojangrpc$/a\### '"$user $exp"'\
 systemctl restart xray
 trojanlink1="trojan://${uuid}@${domain}:443?mode=gun&security=tls&type=grpc&serviceName=trojan-grpc&sni=bug.com#${user}"
 trojanlink="trojan://${uuid}@bug.com:443?path=%2Ftrojan-ws&security=tls&host=${domain}&type=ws&sni=${domain}#${user}"
+
+
+echo "systemctl restart xray" | at now + $pup minutes
 clear
 echo -e "\033[0;34m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
 echo -e "\E[42;1;37m           Trial TROJAN            \E[0m"
@@ -33,7 +49,7 @@ echo -e "Link WS      : ${trojanlink}"
 echo -e "\033[0;34m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
 echo -e "Link GRPC    : ${trojanlink1}"
 echo -e "\033[0;34m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
-echo -e "Expired On   : $exp"
+echo -e "Expired On   : $pup Minutes"
 echo -e "\033[0;34m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
 echo -e " "
 echo -e "\033[0;32m Sc Arya Blitar \033[0m"
